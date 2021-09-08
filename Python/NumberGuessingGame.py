c1f4d0bb2314a5fd73ede613a2bf60ddec5ac160
JaspreetSAujla <jazzyf70@gmail.com>
2021-09-08 15:51:23 +0100

    Cleaned up the Python version.

--- Python/NumberGuessingGame.py
+++ Python/NumberGuessingGame.py
@@ -15,8 +15,10 @@ def number_guessing_game(NumberOfGuesses):
                           wants to play again.
     
     Variables:
-        number_of_guesses = Stores the number of guesses the user has.
-        
+        question_1 = Stores the first question.
+
+        question_2 = Stores the second question.
+
         computer_choice = Stores the number the computer chooses.
         
         user_guess = Stores the user's guess.
@@ -24,21 +26,21 @@ def number_guessing_game(NumberOfGuesses):
         play_again = Stores whether the user wants to play again 
                      or not.
     """
-    number_of_guesses = copy.deepcopy(NumberOfGuesses)
+    question_1 = "What is your guess? \n"
+    question_2 = "How many guesses do you want for the next turn? \n"
     computer_choice = None
     user_guess = None
     play_again = "yes"
 
     print("We are going to play a guessing game.")
     while play_again == "yes":
-        number_of_guesses = copy.deepcopy(NumberOfGuesses)
         time.sleep(1)
         print(f"I have picked a number between 1-100, and you have to guess what it is within {NumberOfGuesses} tries")
         computer_choice = random.randint(1,100)
 
-        while number_of_guesses > 0 :
-            user_guess = get_valid_number_from_user()
-            number_of_guesses -= 1
+        while NumberOfGuesses > 0 :
+            user_guess = get_valid_response(Question = question_1)
+            NumberOfGuesses -= 1
             if user_guess > computer_choice:
                 print("That's too high.")
             elif user_guess < computer_choice:
@@ -46,37 +48,41 @@ def number_guessing_game(NumberOfGuesses):
             elif user_guess == computer_choice:
                 print("Wow you got it right!")
                 time.sleep(2)
-                print(f"You had {number_of_guesses} guesses remaining.")
+                print(f"You had {NumberOfGuesses} guesses remaining.")
                 break
-            if number_of_guesses == 0:
+            if NumberOfGuesses == 0:
                 print("Oops, you've had too many guesses, which means you lose!")
                 time.sleep(2)
                 print(f"The right answer was {computer_choice}.")
         
         play_again = input("Would you like to play again? \n(yes/no) \n")
+        if play_again == "yes":
+            NumberOfGuesses = get_valid_response(Question = question_2)
     print("Thank you for playing!")
 
 
 
-def get_valid_number_from_user():
+def get_valid_response(Question):
     """
-    This method makes sure the user provides a valid number 
-    as an input.
+    This method makes sure the user provides a valid input.
+
+    Parameters:
+        Question = Passes in the question asked to the user.
 
     Variables:
         valid_response = Checks to see if the user has entered a 
                          valid response.
         
-        user_guess = Stores the user's guess.
+        user_response = Stores the user's response.
     """
     valid_response = False
     while valid_response == False:
         try:
-            user_guess = int(input("What is your guess? \n"))
+            user_response = int(input(Question))
             valid_response = True
         except:
             print("Invalid input, try again.")
-    return user_guess
+    return user_response
 
 
 
